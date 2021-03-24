701051ea6bb6bf5f09ef46a85b664d5ba8cf7053
Asim-Tahir <AsimTahir@protonmail.com>
2021-03-24 12:40:20 +0300

    index.postcss updated to index.css

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
-import { createApp } from 'vue'
-import { createHead } from '@vueuse/head'
+import { createApp } from "vue";
+import { createHead } from "@vueuse/head";
+
+import "./styles/index.css";
 
 import App from "./App.vue";
-import router from "./router";
-import "./styles/index.postcss";
+import { router } from "./router";
+import "./styles/index.css";
 
-createApp(App)
-  .use(router)
-  .use(createHead())
-  .mount("#app");
+createApp(App).use(router).use(createHead()).mount("#app");
