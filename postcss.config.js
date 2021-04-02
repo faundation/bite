b18cdc7e2fb1c37bfe17bf53da5c0379fc785aca
Asim-Tahir <AsimTahir@protonmail.com>
2021-04-02 16:25:19 +0300

    Config update

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,25 @@
 module.exports = {
   plugins: [
     require("tailwindcss"),
+    require("@tailwindcss/jit"),
     require("autoprefixer"),
 
     require("postcss-advanced-variables")({
       disable: "@import",
     }),
     require("postcss-preset-env")({
-      stage: 2,
+      stage: 1,
       features: {
         "nesting-rules": true,
-        "color-mod-function": { unresolved: "warn" },
-        "custom-media-queries": true,
+        "all-property": true,
+        "double-position-gradients": true,
+        "gap-properties": true,
+        "gray-function": true,
+        "hexadecimal-alpha-notation": true,
+        "image-set-function": true,
+        "media-query-ranges": true,
+        "system-ui-font-family": true,
       },
     }),
-    require("postcss-nested"),
-    require("postcss-nested-ancestors"),
   ],
 };
